3f63e603fbe9ae7fa376deef66d1fc4e9232dbb2
hyundingi <qo0723@naver.com>
2025-01-14 18:27:39 +0900

    0114_09 추가

---
+++ j0114/src/j0114/Ja0114_09.java
@@ -0,0 +1,29 @@
+package j0114;
+
+public class Ja0114_09 {
+
+	public static void main(String[] args) {
+		
+		int score1 = 0;
+		int score2 = 1;
+		int score3 = 2, score4 =3, score5 = 4;
+		
+		int [] score = new int[5];
+		score[0] = 0;
+		score[1] = 1;
+		score[2] = 2;
+		score[3] = 3;
+		score[4] = 4;
+		
+		for (int i=0;i<5;i++) {
+			System.out.println(score[i]);
+		}
+		
+//		System.out.println(score[1]);
+//		System.out.println(score[2]);
+//		System.out.println(score[3]);
+//		System.out.println(score[4]);
+
+	}
+
+}
